1b1a94f64a17e0d04d019c1f9b50f78872aa0cf1
ICFA <87987489+ICFA@users.noreply.github.com>
2024-06-01 17:13:47 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,10 @@
 pip install -r requirements.txt
 
-# Convert static asset files
 python manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
+python manage.py makemigrations
+
 python manage.py migrate
+
+# python -m gunicorn flairest_project.asgi:application -k uvicorn.workers.UvicornWorker - off
+# python manage.py runserver - on
