5ec2ce6af70c664cdc86306b69e66105f2bd21dc
ICFA <87987489+ICFA@users.noreply.github.com>
2024-06-01 15:49:02 +0500

    deploy

--- flairest_campus/urls.py
+++ flairest_campus/urls.py
@@ -2,28 +2,6 @@ from django.urls import path
 from flairest_campus import views
 from flairest_campus.models import LogMessage
 
-'''
-home_list_view = views.HomeListView.as_view(
-    queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
-    context_object_name="message_list",
-    template_name="flairest_campus/home.html",
-)
-
-home_list_view = views.HomeListView.as_view(
-    context_object_name="message_list",
-    template_name="flairest_campus/index2.html",
-)
-
-urlpatterns = [
-    path("", home_list_view, name="home"), # path("", views.home, name="home"),
-    path("hello/<name>", views.hello_there, name="hello_world"),
-    path("about/", views.about, name="about"),
-    path("contact/", views.contact, name="contact"),
-    path("log/", views.log_message, name="log"),
-    path('media/', views.home_page, name="media"),
-    path('uni/', UniCreate.as_view())
-]
-'''
 
 urlpatterns = [
     path('', views.home_page),
