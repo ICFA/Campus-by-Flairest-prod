9dff36c727c9197b99696f3502bfa46d6f7347a7
ICFA <87987489+ICFA@users.noreply.github.com>
2024-06-01 16:11:14 +0500

    media

--- flairest_project/settings.py
+++ flairest_project/settings.py
@@ -130,8 +130,8 @@ STATIC_ROOT = BASE_DIR / 'static_collected'
 # media files (jpg, png)
 ### https://docs.djangoproject.com/en/5.0/howto/
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
