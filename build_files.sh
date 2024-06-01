131773a63285d6c2443646354b114bf0a2d8afb9
ICFA <87987489+ICFA@users.noreply.github.com>
2024-06-01 17:17:33 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,9 +2,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py makemigrations
+# python manage.py makemigrations
 
-python manage.py migrate
+# python manage.py migrate
 
 # python -m gunicorn flairest_project.asgi:application -k uvicorn.workers.UvicornWorker - off
 # python manage.py runserver - on
